58296440c1f7a93a6b8bb6a6ad6f826fbac21655
treeform <starplant@gmail.com>
2022-01-09 19:17:17 -0800

    Multiple windows example: Only display on mouse move.

--- examples/multiple_windows.nim
+++ examples/multiple_windows.nim
@@ -7,8 +7,8 @@ let
 var i: int
 proc display(window: Window, bxy: Boxy) =
   makeContextCurrent(window)
-  bxy.beginFrame(windowSize)
-  bxy.drawRect(rect(vec2(0, 0), windowSize.vec2), color(1, 1, 1, 1))
+  bxy.beginFrame(window.size)
+  bxy.drawRect(rect(vec2(0, 0), window.size.vec2), color(1, 1, 1, 1))
   bxy.drawImage("rhino", vec2((i mod windowSize.x).float32, 0))
   bxy.endFrame()
   window.swapBuffers()
@@ -17,6 +17,10 @@ proc display(window: Window, bxy: Boxy) =
 let
   window1 = newWindow("Windy1 + Boxy", windowSize)
   window2 = newWindow("Windy2 + Boxy", windowSize)
+
+var
+  dirty1: bool
+  dirty2: bool
 
 makeContextCurrent(window1)
 loadExtensions()
@@ -30,7 +34,21 @@ loadExtensions()
 let bxy2 = newBoxy()
 bxy2.addImage("rhino", rhino)
 
-while not window1.closeRequested:
+window1.onMouseMove = proc() =
+  echo "move 1", window1.mouseDelta
+  dirty1 = true
+
+window2.onMouseMove = proc() =
+  echo "move 2", window2.mouseDelta
+  dirty2 = true
+
+while not window1.closeRequested and not window2.closeRequested:
   pollEvents()
-  display(window1, bxy1)
-  display(window2, bxy2)
+
+  if dirty1:
+    dirty1 = false
+    display(window1, bxy1)
+
+  if dirty2:
+    dirty2 = false
+    display(window2, bxy2)
